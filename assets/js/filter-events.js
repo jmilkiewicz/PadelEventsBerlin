058362614a78bf955abd370c49256158a08aecb5
Jakub Milkiewicz <jmilkiewicz@softmil.pl>
2023-07-29 09:39:22 +0200

    now using chatgpt code

--- assets/js/filter-events.js
+++ assets/js/filter-events.js
@@ -1,14 +1,16 @@
+
 function hideEventsBefore(beforeDate) {
-  const events = document.getElementById("events-list").getElementsByTagName('li');
-  for (let event of events) {
-    const dateAttribute = event.getAttribute("event-date")
-    if (dateAttribute) {
+  const eventItems = document.querySelectorAll('.events-list li');
+  
+  eventItems.forEach(item => {
+    const dateAttribute = item.getAttribute("event-date")
+    if(dateAttribute){
       const dateEvent = Date.parse(dateAttribute)
       if(dateEvent < beforeDate){
-        event.style.display = 'none';
+        item.style.display = 'none';
       }
     }
-  }
+  });
 }
 
 function hideByType(){
