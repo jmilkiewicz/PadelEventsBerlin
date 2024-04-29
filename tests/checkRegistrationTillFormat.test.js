89af572776a6b2f701dff2d48bcfafbe40baddd3
Jakub Milkiewicz <jmilkiewicz@softmil.pl>
2024-04-29 12:37:01 +0200

    adding test to check registraton date - if date in wrong format it does not work on mobile

---
+++ tests/checkRegistrationTillFormat.test.js
@@ -0,0 +1,25 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+const re =new RegExp(  "^(?<year>-?(?:[1-9][0-9]*)?[0-9]{4})-(?<month>1[0-2]|0[1-9])-(?<day>3[01]|0[1-9]|[12][0-9])T(?<hour>2[0-3]|[01][0-9]):(?<minute>[0-5][0-9])(?<timezone>Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$")
+function isValidDate(stringDate) {
+  return re.test(stringDate)
+}
+
+test('check registration date format', () => {
+  const eventsYamlFile = yaml.load(fs.readFileSync('_data/events.yaml', 'utf8'));
+
+  const x = eventsYamlFile.map(x => [x["registration_till"], x["name"]])
+    .filter(x => x[0])
+    .filter(x => !isValidDate(x[0]))
+    .reduce((acc, cur) => {
+      if (isValidDate(cur[0])) {
+        return acc
+      } else {
+        acc.push(cur);
+        return acc
+      }
+    }, [])
+
+  expect(x).toEqual([]);
+});
